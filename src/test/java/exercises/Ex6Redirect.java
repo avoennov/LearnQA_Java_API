51b24ba39fe09ed70b2bf9f3c2956a2b3a2e0f50
avoennov <52177854+avoennov@users.noreply.github.com>
2022-09-16 00:41:21 +0300

    Ex6: Redirect

---
+++ src/test/java/exercises/Ex6Redirect.java
@@ -0,0 +1,29 @@
+package exercises;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+
+public class Ex6Redirect {
+
+    @Test
+    /*
+    Необходимо написать тест, который создает GET-запрос на адрес: https://playground.learnqa.ru/api/long_redirect
+    С этого адреса должен происходить редирект на другой адрес. Наша задача — распечатать адрес, на который редиректит указанные URL.
+    Ответом должна быть ссылка на тест в вашем репозитории.
+    */
+
+    public void testRedirect(){
+
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/long_redirect")
+                .andReturn();
+
+        int statusCode = response.getStatusCode();
+        System.out.println(statusCode);
+
+        String newUrl = response.getHeader("X-Host");
+        System.out.println(newUrl);
+
+    }
+}
