12211a1eb3362cdeb3342d0b1bde28d9dc6f76b2
avoennov <52177854+avoennov@users.noreply.github.com>
2022-10-26 21:22:38 +0300

    Part 4. Create framework

---
+++ src/test/java/helloWorld/HelloWorldTest.java
@@ -0,0 +1,193 @@
+package helloWorld;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Headers;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class HelloWorldTest {
+
+    @Test
+    public void testHelloWorld(){
+        //System.out.println("Hello world");
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/hello")
+                .andReturn();
+        response.prettyPrint();
+    }
+
+    @Test
+    public void testRestAssured(){
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "John");
+
+        JsonPath response = RestAssured
+                .given()
+                .queryParams(params)
+                .get("https://playground.learnqa.ru/api/hello")
+                .jsonPath();
+        String name = response.get("answer2");
+        if(name == null){
+            System.out.println("The key 'answer2' is absent");
+        } else {
+            System.out.println(name);
+        }
+    }
+
+    @Test
+    public void testRestAssured2(){
+        Map<String,Object> body = new HashMap<>();
+        body.put("param1","value1");
+        body.put("param2","value2");
+
+        Response response = RestAssured
+                .given()
+                .body(body)
+                .post("https://playground.learnqa.ru/api/check_type")
+                .andReturn();
+        response.print();
+    }
+
+    @Test
+    public void testRestAssured3(){
+        Response response = RestAssured
+                .given()
+                .redirects()
+                .follow(true)
+                .when()
+                .get("https://playground.learnqa.ru/api/get_303")
+                .andReturn();
+
+        int statusCode = response.getStatusCode();
+        System.out.println(statusCode);
+    }
+
+    @Test
+    public void testRestAssured4(){
+        Map<String,String> headers = new HashMap<>();
+        headers.put("myHeader1","myValue1");
+        headers.put("myHeader2","myValue2");
+
+        Response response = RestAssured
+                .given()
+                .redirects()
+                .follow(false)
+                .when()
+                .get("https://playground.learnqa.ru/api/get_303")
+                .andReturn();
+
+        response.prettyPrint();
+
+        String locationHeader = response.getHeader("Location");
+        System.out.println(locationHeader);
+    }
+
+    @Test
+    public void testRestAssured5(){
+        Map<String,String> data = new HashMap<>();
+        data.put("login","secret_login2");
+        data.put("password","secret_pass2");
+
+        Response response = RestAssured
+                .given()
+                .body(data)
+                .when()
+                .post("https://playground.learnqa.ru/api/get_auth_cookie")
+                .andReturn();
+
+        System.out.println("\nPretty text:");
+        response.prettyPrint();
+
+        System.out.println("\nHeaders:");
+        Headers responseHeaders = response.getHeaders();
+        System.out.println(responseHeaders);
+
+        System.out.println("\nCookies:");
+        Map<String,String> responseCookies = response.getCookies();
+        System.out.println(responseCookies);
+
+        /*String responseCookie = response.getCookie("auth_cookie");
+        System.out.println(responseCookie);*/
+    }
+
+    @Test
+    public void testRestAssured6(){
+        Map<String,String> data = new HashMap<>();
+        data.put("login","secret_login");
+        data.put("password","secret_pass");
+
+        Response responseForGet = RestAssured
+                .given()
+                .body(data)
+                .when()
+                .post("https://playground.learnqa.ru/api/get_auth_cookie")
+                .andReturn();
+
+        String responseCookie = responseForGet.getCookie("auth_cookie");
+        Map<String,String> cookies = new HashMap<>();
+        cookies.put("auth_cookie", responseCookie);
+
+        Response responseForCheck = RestAssured
+                .given()
+                .body(data)
+                .cookies(cookies)
+                .when()
+                .post("https://playground.learnqa.ru/api/check_auth_cookie")
+                .andReturn();
+
+        responseForCheck.print();
+    }
+
+    @Test
+    public void testRestAssured7(){
+        Map<String,String> data = new HashMap<>();
+        data.put("login","secret_login2");
+        data.put("password","secret_pass2");
+
+        Response responseForGet = RestAssured
+                .given()
+                .body(data)
+                .when()
+                .post("https://playground.learnqa.ru/api/get_auth_cookie")
+                .andReturn();
+
+        String responseCookie = responseForGet.getCookie("auth_cookie");
+
+        Map<String,String> cookies = new HashMap<>();
+        if(responseCookie != null){
+            cookies.put("auth_cookie", responseCookie);
+        }
+
+        Response responseForCheck = RestAssured
+                .given()
+                .body(data)
+                .cookies(cookies)
+                .when()
+                .post("https://playground.learnqa.ru/api/check_auth_cookie")
+                .andReturn();
+
+        responseForCheck.print();
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/test/java/helloWorld/HelloWorldTest2.java
@@ -0,0 +1,27 @@
+package helloWorld;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HelloWorldTest2 {
+
+
+    @Test
+    public void testFor200(){
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/map")
+                .andReturn();
+        assertEquals(200, response.statusCode(),"Unexpected status code");
+    }
+
+    @Test
+    public void testFor404(){
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/map2")
+                .andReturn();
+        assertEquals(404, response.statusCode(),"Unexpected status code");
+    }
+}
