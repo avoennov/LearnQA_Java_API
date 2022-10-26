fb0ff36cc71d170bbb22f6746071e29f1c241771
avoennov <52177854+avoennov@users.noreply.github.com>
2022-10-27 00:44:24 +0300

    Ex16: Requesting another user's data

--- src/test/java/tests/UserGetTest.java
+++ src/test/java/tests/UserGetTest.java
@@ -47,4 +47,39 @@ public class UserGetTest extends BaseTestCase {
         String[] expectedFields = {"username", "firstName", "lastName", "email"};
         Assertions.assertJsonHasFields(responseUserData, expectedFields);
     }
+
+    /*
+    -----------[Ex16: Запрос данных другого пользователя]--------------------
+
+    В этой задаче нужно написать тест, который авторизовывается одним пользователем, но получает данные другого
+    (т.е. с другим ID). И убедиться, что в этом случае запрос также получает только username, так как мы не должны
+    видеть остальные данные чужого пользователя.
+    */
+    @Test
+    public void testGetUserDetailsAuthAsAnotherUser() {
+        Map<String, String> authData = new HashMap<>();
+        authData.put("email", "vinkotov@example.com");
+        authData.put("password", "1234");
+
+        Response responseGetAuth = RestAssured
+                .given()
+                .body(authData)
+                .post("https://playground.learnqa.ru/api/user/login")
+                .andReturn();
+
+        String header = this.getHeader(responseGetAuth, "x-csrf-token");
+        String cookie = this.getCookie(responseGetAuth, "auth_sid");
+
+        Response responseUserData = RestAssured
+                .given()
+                .header("x-csrf-token", header)
+                .cookies("auth_sid", cookie)
+                .get("https://playground.learnqa.ru/api/user/1")
+                .andReturn();
+
+        Assertions.assertJsonHasField(responseUserData, "username");
+        Assertions.assertJsonHasNotField(responseUserData, "firstName");
+        Assertions.assertJsonHasNotField(responseUserData, "lastName");
+        Assertions.assertJsonHasNotField(responseUserData, "email");
+    }
 }
